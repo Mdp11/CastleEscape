2fa0a4308a8ee809f8563a218777d070d938a124
Mattia De Prisco <mattia.dipi@gmail.com>
2020-07-09 10:38:52 +0200

    Initial commit

---
+++ Source/CastleEscape/CastleEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CastleEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASTLEESCAPE_API ACastleEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
