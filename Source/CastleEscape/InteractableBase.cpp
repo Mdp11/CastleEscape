0d67b617feddf6d451a8ff5a8ee98782515b0a90
Mattia De Prisco <mattia.dipi@gmail.com>
2020-07-15 11:31:34 +0200

    Created base class for all interactable objects

---
+++ Source/CastleEscape/InteractableBase.cpp
@@ -0,0 +1,33 @@
+// Copyrights Mattia De Prisco 2020
+
+
+#include "InteractableBase.h"
+
+// Sets default values
+AInteractableBase::AInteractableBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractableBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInteractableBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractableBase::Interact()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Do not use this class directly, inherit from it instead."))
+}
+
+
---
+++ Source/CastleEscape/InteractableBase.h
@@ -0,0 +1,30 @@
+// Copyrights Mattia De Prisco 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableBase.generated.h"
+
+UCLASS()
+class CASTLEESCAPE_API AInteractableBase : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AInteractableBase();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere)
+    UStaticMeshComponent* StaticMeshComponent{nullptr};
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    virtual void Interact();
+};
