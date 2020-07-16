4027e1b4b8849fbf98cfd1935c3a50a35c7cc029
Mattia De Prisco <mattia.dipi@gmail.com>
2020-07-16 11:37:03 +0200

    Created cell door class (still no opening function)

---
+++ Source/CastleEscape/CellDoor.cpp
@@ -0,0 +1,32 @@
+// Copyrights Mattia De Prisco 2020
+
+
+#include "CellDoor.h"
+
+
+#include "Utilities.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/StaticMeshComponent.h"
+
+ACellDoor::ACellDoor()
+{
+    StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+    const auto CellDoorMesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(
+        TEXT("StaticMesh'/Game/MedievalDungeon/Meshes/Architecture/Dungeon/SM_Cell_Door.SM_Cell_Door'"));
+    if (CellDoorMesh.Object)
+    {
+        StaticMeshComponent->SetStaticMesh(CellDoorMesh.Object);
+    }
+    else
+    {
+        UNDEF_PTR("CellDoor mesh");
+    }
+}
+
+void ACellDoor::Interact()
+{
+    
+}
+
+
+
---
+++ Source/CastleEscape/CellDoor.h
@@ -0,0 +1,27 @@
+// Copyrights Mattia De Prisco 2020
+
+#pragma once
+
+#include "CellLock.h"
+#include "CoreMinimal.h"
+#include "InteractableBase.h"
+#include "CellDoor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CASTLEESCAPE_API ACellDoor : public AInteractableBase
+{
+    GENERATED_BODY()
+
+public:
+    ACellDoor();
+    
+    void Interact() override final;
+
+private:
+    UPROPERTY(EditAnywhere)
+    ACellLock* CellLock{nullptr};
+    bool Open{false};
+};
